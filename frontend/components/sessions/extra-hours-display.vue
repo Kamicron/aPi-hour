133dd90a92e3111d3f6d155aa517d34088625761
Kamicron <ludovic.chevroulet@outlook.fr>
2025-01-14 23:42:11 +0100

    fix: full responsive extra-hours

--- frontend/components/sessions/extra-hours-display.vue
+++ frontend/components/sessions/extra-hours-display.vue
@@ -1,53 +1,137 @@
 <template>
   <div class="extra-hours-display">
-    <div class="extra-hours-display__header">
-      <h2>Calcul des heures supplémentaires</h2>
+    <div class="extra-hours-display__ultra-small">
+      <button class="btn" @click="toggleDisplayView = true">
+        <i class="fa-solid fa-clock"></i>
+      </button>
     </div>
 
-    <div class="extra-hours-display__inputs">
-      <label class="extra-hours-display__label">
-        Date de début :
-        <input type="date" v-model="startDate" @change="fetchExtraHours" class="pi-input" />
-      </label>
-      <label class="extra-hours-display__label">
-        Date de fin :
-        <input type="date" v-model="endDate" @change="fetchExtraHours" class="pi-input" />
-      </label>
+    <div class="extra-hours-display__small">
+      <h2>Heures supp.</h2>
+      <div class="extra-hours-display__content">
+        <div v-if="loading" class="extra-hours-display__loading">
+          <pi-loader />
+        </div>
+        <div v-if="!loading && !error" class="extra-hours-display__table">
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Total</span>
+            <span
+              class="extra-hours-display__cell"
+              :class="{
+                'extra-hours-display__positive': extraHours >= 0,
+                'extra-hours-display__negative': extraHours < 0,
+              }"
+            >
+              {{ extraHours.toFixed(2) }} h
+            </span>
+          </div>
+        </div>
+        <div v-if="error" class="extra-hours-display__error">{{ error }}</div>
+      </div>
     </div>
 
-    <div class="extra-hours-display__content">
-      <div v-if="loading" class="extra-hours-display__loading">
-        <pi-loader />
+    <div class="extra-hours-display__large">
+      <div class="extra-hours-display__header">
+        <h2>Calcul des heures supplémentaires</h2>
       </div>
-      <div v-if="!loading && !error" class="extra-hours-display__table">
-        <div class="extra-hours-display__row">
-          <span class="extra-hours-display__cell">Heures Travaillées</span>
-          <span class="extra-hours-display__cell">{{ workedHours.toFixed(2) }} h</span>
-        </div>
-        <div class="extra-hours-display__row">
-          <span class="extra-hours-display__cell">Heures de Pause</span>
-          <span class="extra-hours-display__cell">{{ pauseHours.toFixed(2) }} h</span>
-        </div>
-        <div class="extra-hours-display__row">
-          <span class="extra-hours-display__cell">Heures Contractuelles</span>
-          <span class="extra-hours-display__cell">{{ contractualHours.toFixed(2) }} h</span>
-        </div>
-        <div class="extra-hours-display__row">
-          <span class="extra-hours-display__cell">Heures Supplémentaires</span>
-          <span
-            class="extra-hours-display__cell"
-            :class="{
-              'extra-hours-display__positive': extraHours >= 0,
-              'extra-hours-display__negative': extraHours < 0,
-            }"
-          >
-            {{ extraHours.toFixed(2) }} h
-          </span>
-        </div>
+
+      <div class="extra-hours-display__inputs">
+        <label class="extra-hours-display__label">
+          Date de début :
+          <input type="date" v-model="startDate" @change="fetchExtraHours" class="pi-input" />
+        </label>
+        <label class="extra-hours-display__label">
+          Date de fin :
+          <input type="date" v-model="endDate" @change="fetchExtraHours" class="pi-input" />
+        </label>
       </div>
-      <div v-if="error" class="extra-hours-display__error">{{ error }}</div>
+
+      <div class="extra-hours-display__content">
+        <div v-if="loading" class="extra-hours-display__loading">
+          <pi-loader />
+        </div>
+        <div v-if="!loading && !error" class="extra-hours-display__table">
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Travaillées</span>
+            <span class="extra-hours-display__cell">{{ workedHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures de Pause</span>
+            <span class="extra-hours-display__cell">{{ pauseHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Contractuelles</span>
+            <span class="extra-hours-display__cell">{{ contractualHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Supplémentaires</span>
+            <span
+              class="extra-hours-display__cell"
+              :class="{
+                'extra-hours-display__positive': extraHours >= 0,
+                'extra-hours-display__negative': extraHours < 0,
+              }"
+            >
+              {{ extraHours.toFixed(2) }} h
+            </span>
+          </div>
+        </div>
+        <div v-if="error" class="extra-hours-display__error">{{ error }}</div>
+      </div>
     </div>
   </div>
+
+  <modal v-model="toggleDisplayView" title="Heures supplémentaires">
+    <div class="extra-hours-display__modal">
+      <div class="extra-hours-display__header">
+        <h2>Calcul des heures supplémentaires</h2>
+      </div>
+
+      <div class="extra-hours-display__inputs">
+        <label class="extra-hours-display__label">
+          Date de début :
+          <input type="date" v-model="startDate" @change="fetchExtraHours" class="pi-input" />
+        </label>
+        <label class="extra-hours-display__label">
+          Date de fin :
+          <input type="date" v-model="endDate" @change="fetchExtraHours" class="pi-input" />
+        </label>
+      </div>
+
+      <div class="extra-hours-display__content">
+        <div v-if="loading" class="extra-hours-display__loading">
+          <pi-loader />
+        </div>
+        <div v-if="!loading && !error" class="extra-hours-display__table">
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Travaillées</span>
+            <span class="extra-hours-display__cell">{{ workedHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures de Pause</span>
+            <span class="extra-hours-display__cell">{{ pauseHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Contractuelles</span>
+            <span class="extra-hours-display__cell">{{ contractualHours.toFixed(2) }} h</span>
+          </div>
+          <div class="extra-hours-display__row">
+            <span class="extra-hours-display__cell">Heures Supplémentaires</span>
+            <span
+              class="extra-hours-display__cell"
+              :class="{
+                'extra-hours-display__positive': extraHours >= 0,
+                'extra-hours-display__negative': extraHours < 0,
+              }"
+            >
+              {{ extraHours.toFixed(2) }} h
+            </span>
+          </div>
+        </div>
+        <div v-if="error" class="extra-hours-display__error">{{ error }}</div>
+      </div>
+    </div>
+  </modal>
 </template>
 
 <script setup lang="ts">
@@ -62,7 +146,7 @@ const token = useCookie("token");
 // ------------------
 
 // ---- Reactive ----
-const startDate = ref<string>(""); 
+const startDate = ref<string>("");
 const endDate = ref<string>("");
 const loading = ref(false);
 const error = ref<string | null>(null);
@@ -70,15 +154,16 @@ const workedHours = ref<number>(0);
 const pauseHours = ref<number>(0);
 const contractualHours = ref<number>(0);
 const extraHours = ref<number>(0);
+const toggleDisplayView = ref<boolean>(false);
 // ------------------
 
 // --- Async Func --- 
-const fetchExtraHours = async () => { 
+const fetchExtraHours = async () => {
   if (!startDate.value || !endDate.value) {
     error.value = "Veuillez sélectionner les deux dates.";
-    return; 
+    return;
   }
-  loading.value = true; 
+  loading.value = true;
   error.value = null;
   try {
     const response = await $api.post(
@@ -93,8 +178,8 @@ const fetchExtraHours = async () => {
         },
       }
     );
-    const data = response.data; 
-    workedHours.value = data.workedHours; 
+    const data = response.data;
+    workedHours.value = data.workedHours;
     pauseHours.value = data.pauseHours;
     contractualHours.value = data.contractualHours;
     extraHours.value = data.extraHours;
@@ -104,9 +189,9 @@ const fetchExtraHours = async () => {
     } else {
       error.value = "Une erreur est survenue lors de la récupération des données.";
     }
-    console.error(err); 
+    console.error(err);
   } finally {
-    loading.value = false; 
+    loading.value = false;
   }
 };
 </script>
@@ -114,13 +199,21 @@ const fetchExtraHours = async () => {
 <style lang="scss" scoped>
 .extra-hours-display {
   color: $color-text-primary;
-  padding: $spacing-large;
-  min-width: 420px;
+  width: 100%;
+  height: inherit !important;
+  box-sizing: border-box;
+  position: relative;
+  container-type: size;
+  container-name: extra-hours-display;
+
+  h2 {
+    text-align: center;
+    margin-bottom: $spacing-medium;
+  }
 
   &__header {
     text-align: center;
     margin-bottom: $spacing-large;
-
   }
 
   &__inputs {
@@ -150,7 +243,7 @@ const fetchExtraHours = async () => {
   }
 
   &__row {
-    display: contents; /* Permet à chaque cellule d'occuper son espace */
+    display: contents;
   }
 
   &__cell {
@@ -162,14 +255,14 @@ const fetchExtraHours = async () => {
       text-align: right;
       font-weight: bold;
     }
+  }
 
-    &.extra-hours-display__positive {
-      color: $color-success;
-    }
+  &__positive {
+    color: $color-success;
+  }
 
-    &.extra-hours-display__negative {
-      color: $color-danger;
-    }
+  &__negative {
+    color: $color-danger;
   }
 
   &__error {
@@ -177,6 +270,61 @@ const fetchExtraHours = async () => {
     font-weight: bold;
     font-size: $font-size-base;
     margin-top: $spacing-medium;
+  }
+
+  // États par défaut
+  &__ultra-small {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+
+  &__small {
+    display: none;
+  }
+
+  &__large {
+    display: none;
+  }
+
+  // Règles de container
+  @container extra-hours-display ((min-height: 70px) and (max-height: 245px)) and (min-width: 200px) {
+    .extra-hours-display {
+      padding: 0;
+    }
+
+    .extra-hours-display__ultra-small {
+      display: none;
+    }
+
+    .extra-hours-display__small {
+      display: block;
+    }
+  }
+
+  @container extra-hours-display (min-height: 245px) and (min-width: 300px) {
+    .extra-hours-display__large {
+      display: block;
+    }
+
+    .extra-hours-display__small {
+      display: none;
+    }
+
+    .extra-hours-display__ultra-small {
+      display: none;
+    }
+  }
+}
+
+// Style pour la modale
+.extra-hours-display__modal {
+  padding: $spacing-large;
+
+  h2 {
+    margin-bottom: $spacing-large;
   }
 }
 </style>
